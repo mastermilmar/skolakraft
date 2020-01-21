f8be8dc45b9084ed6568e30b730939c46c94a340
Miloš Milovanović <misabrzi@gmail.com>
2020-01-21 11:45:47 +0100

    Meni ispravke

--- js/meni.js
+++ js/meni.js
@@ -14,9 +14,9 @@ window.onscroll = function() { scrollFunction() };
 function scrollFunction() {
 	if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
 		document.getElementById("nav").style.margin = "0em";
-		document.getElementById("logo").style.margin = "0";
+		document.getElementById("logo").style["margin-top"] = "0";
 	} else {
 		document.getElementById("nav").style.margin = "1em 0";
-		document.getElementById("logo").style.margin = "0";
+		document.getElementById("logo").style["margin-top"] = "1.5rem";
 	}
 }
